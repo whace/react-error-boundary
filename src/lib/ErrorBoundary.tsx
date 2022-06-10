e90790314fa497f3e10142a6ba3e31cf376657fd
osAce <381187081@qq.com>
2022-06-10 13:34:11 +0800

    feat: init

---
+++ src/lib/ErrorBoundary.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+// 出错后显示组件的 props
+type FallbackElement = React.ReactElement<unknown,string|React.FC | typeof React.Component> | null;
+
+interface ErrorBoundaryProps {
+  fallback?: FallbackElement;
+}
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // 更新 state 使下一次渲染能够显示降级后的 UI
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // 你同样可以将错误日志上报给服务器
+    logger.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // 你可以自定义降级后的 UI 并渲染
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
+
+export {
+  ErrorBoundary
+}
